238ad3440e34a800b03c55f72e4539eddee2e9c0
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2024-06-21 20:02:14 +0100

    FEAT: Implement CEF logging and host ip

--- FileGateway/app/api/endpoints.py
+++ FileGateway/app/api/endpoints.py
@@ -1,26 +1,55 @@
-from fastapi import APIRouter, UploadFile, Request, HTTPException
+from fastapi import APIRouter, UploadFile, Request, HTTPException, Response
 from app.services import file_service, connection_service
-from app.schemas.schemas import Response, FileInformation, ConnectionInformation
+from app.schemas.schemas import (
+    Response,
+    FileInformation,
+    ConnectionInformation,
+    CommonEventFormat,
+)
 import requests
+from requests import Response as APIResponse
+import logging
+from uuid import uuid4
+
 
 router = APIRouter()
 
+
 @router.get("/")
 async def root():
-
     return {"message": "Default"}
+
 
 @router.post("/upload")
 async def file_upload(request: Request, file: UploadFile) -> Response:
-    connection_info: ConnectionInformation = connection_service.connection_info(request=request)
+    log: CommonEventFormat = CommonEventFormat(version="0.1.0")
+    log_info = {}
+    connection_info: ConnectionInformation = connection_service.connection_info(
+        request=request
+    )
     file_info: FileInformation = file_service.get_metadata(file=file)
     
-    requests.post(url="http://localhost:8000/saveFile",
-                  data={"file_id": file_info.file_id, "filename": file_info.filename, "filesize": file_info.filesize},
-                  files={"file": file.file})
-        
-    response = Response(message="POST Upload", 
-                        file_info=file_info, 
-                        connection_info=connection_info)
+    log_info["src"] = connection_info.source_addr
+
+    store_file: APIResponse = requests.post(
+        url="http://localhost:8000/saveFile",
+        data={
+            "file_id": file_info.file_id,
+            "filename": file_info.filename,
+            "filesize": file_info.filesize,
+        },
+        files={"file": (file.filename, file.file, file.content_type)},
+    )
+
+    if store_file.status_code == 201:
+        logging.debug("File store request was successful")
+        log.event = "File store request successful"
+        log.severity = 1
+        log.log_id = "L0001"
+        log.log()
+
+    response = Response(
+        message="POST Upload", file_info=file_info, connection_info=connection_info
+    )
 
     return response
